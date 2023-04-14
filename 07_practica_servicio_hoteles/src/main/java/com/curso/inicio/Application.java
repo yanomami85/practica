7640797d3fc77f4f2672106cfc0fd74ceb308203
yanomami85 <jjvga@yahoo.com>
2023-04-14 08:23:02 +0200

    Reservas no recupera la configuración del servidor

--- 07_practica_servicio_hoteles/src/main/java/com/curso/inicio/Application.java
+++ 07_practica_servicio_hoteles/src/main/java/com/curso/inicio/Application.java
@@ -2,8 +2,12 @@ package com.curso.inicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan(basePackages = "com.curso.model")
+@EnableJpaRepositories(basePackages = "com.curso.repository")
+@SpringBootApplication(scanBasePackages = {"com.curso.controller", "com.curso.service"})
 public class Application {
 
 	public static void main(String[] args) {
--- 07_practica_servicio_reservas/src/main/java/com/curso/inicio/Application.java
+++ 07_practica_servicio_reservas/src/main/java/com/curso/inicio/Application.java
@@ -2,12 +2,30 @@ package com.curso.inicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@EntityScan(basePackages = "com.curso.model")
+@EnableJpaRepositories(basePackages = "com.curso.repository")
+@SpringBootApplication(scanBasePackages = {"com.curso.controller", "com.curso.service"})
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+        BasicAuthenticationInterceptor interceptor;
+        interceptor = new BasicAuthenticationInterceptor("user3", "user3");
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(interceptor);
+        return restTemplate;
 	}
 
 }
